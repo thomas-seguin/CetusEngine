cce165f5fc9f950dc639f7949615d131a87be6ef
thomas-seguin <seguinthomas7@gmail.com>
2026-08-23 10:40:46 -0400

    created basic system for vertex buffer layouts

--- CetusEngine/src/Core/Application.cpp
+++ CetusEngine/src/Core/Application.cpp
@@ -7,6 +7,25 @@
 namespace Cetus {
     static Application* s_Application = nullptr;
 
+    static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) {
+        switch (type) {
+            case ShaderDataType::Float:  return GL_FLOAT;
+            case ShaderDataType::Float2: return GL_FLOAT;
+            case ShaderDataType::Float3: return GL_FLOAT;
+            case ShaderDataType::Float4: return GL_FLOAT;
+            case ShaderDataType::Mat3:   return GL_FLOAT;
+            case ShaderDataType::Mat4:   return GL_FLOAT;
+            case ShaderDataType::Int:    return GL_INT;
+            case ShaderDataType::Int2:   return GL_INT;
+            case ShaderDataType::Int3:   return GL_INT;
+            case ShaderDataType::Int4:   return GL_INT;
+            case ShaderDataType::Bool:   return GL_BOOL;
+        }
+
+        CT_CORE_ASSERT(false, "Unknown shader data type");
+        return 0;
+    }
+
     Application::Application(const ApplicationSpecification &specification) : m_Specification(specification) {
         s_Application = this;
         Log::Init();
@@ -21,17 +40,37 @@ namespace Cetus {
         glGenVertexArrays(1, &m_VertexArray);
         glBindVertexArray(m_VertexArray);
 
-        float vertices[3*3] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f,
+        float vertices[3*7] = {
+            -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+            0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
         };
 
         m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        {
+            BufferLayout layout = {
+                {ShaderDataType::Float3, "a_Position"},
+                {ShaderDataType::Float4, "a_Color"}
+            };
 
+            m_VertexBuffer->SetLayout(layout);
+        }
+
+        uint32_t index = 0;
+        const auto& layout = m_VertexBuffer->GetLayout();
+        for (const auto& element : layout) {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(
+                    index,
+                    element.GetComponentCount(),
+                    ShaderDataTypeToOpenGLBaseType(element.Type),
+                    element.Normalized ? GL_TRUE : GL_FALSE,
+                    layout.GetStride(),
+                    (const void*)element.Offset
+                );
+            index++;
+        }
 
         uint32_t indices[3] = { 0, 1, 2 };
         m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -41,10 +80,12 @@ namespace Cetus {
             #version 330 core
 
             layout(location = 0) in vec3 a_Position;
+            layout(location = 1) in vec4 a_Color;
 
-
+            out vec4 v_Color;
             void main()
             {
+                v_Color = a_Color;
                 gl_Position = vec4(a_Position, 1.0f);
             }
         )";
@@ -54,10 +95,12 @@ namespace Cetus {
 
             layout(location = 0) out vec4 color;
 
+            in vec4 v_Color;
 
             void main()
             {
                 color = vec4(0.8, 0.2, 0.3, 1.0);
+                color = v_Color;
             }
         )";
 
--- CetusEngine/src/Core/Core.h
+++ CetusEngine/src/Core/Core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Core/Log.h"
+
 #ifdef CT_DEBUG
     #define CT_ENABLE_ASSERTS
 #endif
