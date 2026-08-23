26fa77f4d2d40700d82c5fff4d8276b2ab40dcb0
thomas seguin <seguinthomas7@gmail.com>
2026-08-23 09:47:01 -0400

    started work on graphics context

--- CetusEngine/src/Core/Core.h
+++ CetusEngine/src/Core/Core.h
@@ -4,9 +4,23 @@
     #define CT_ENABLE_ASSERTS
 #endif
 
+#if defined(_MSC_VER)
+    #define CT_DEBUGBREAK() __debugbreak()
+#elif defined(__MINGW32__) || defined(__MINGW64__)
+    #include <windows.h>
+    #define CT_DEBUGBREAK() DebugBreak()
+#elif defined(__clang__)
+    #define CT_DEBUGBREAK() __builtin_debugtrap()
+#elif defined(__GNUC__)
+    #include <csignal>
+    #define CT_DEBUGBREAK() raise(SIGTRAP)
+#else
+    #error "CT_DEBUGBREAK not implemented for this platform/compiler"
+#endif
+
 #ifdef CT_ENABLE_ASSERTS
-    #define CT_ASSERT(x, ...) { if(!(x)) { CT_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-    #define CT_CORE_ASSERT(x, ...) { if(!(x)) { CT_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+    #define CT_ASSERT(x, ...) { if(!(x)) { CT_ERROR("Assertion Failed: {0}", __VA_ARGS__); CT_DEBUGBREAK(); } }
+    #define CT_CORE_ASSERT(x, ...) { if(!(x)) { CT_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); CT_DEBUGBREAK(); } }
 #else
     #define CT_ASSERT(x, ...)
     #define CT_CORE_ASSERT(x, ...)
--- CetusEngine/src/Renderer/GraphicsContext.h
+++ CetusEngine/src/Renderer/GraphicsContext.h
@@ -1,9 +1,15 @@
 #pragma once
 
+struct GLFWwindow;
+
 namespace Cetus {
     class GraphicsContext {
     public:
+        virtual ~GraphicsContext() = default;
         virtual void Init() = 0;
         virtual void SwapBuffers() = 0;
+
+        static void ConfigureWindowHints();
+        static GraphicsContext* Create(GLFWwindow* window);
     };
 }
