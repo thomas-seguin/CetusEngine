1d1e662f200f55893193361423ca01f436da125a
thomas-seguin <seguinthomas7@gmail.com>
2026-08-30 13:59:50 -0400

    extended shader class to upload uniforms to shaders

--- CetusEngine/src/Platform/OpenGL/OpenGLShader.cpp
+++ CetusEngine/src/Platform/OpenGL/OpenGLShader.cpp
@@ -133,4 +133,69 @@ namespace Cetus {
     void OpenGLShader::Unbind() const {
         glUseProgram(0);
     }
+
+
+    void OpenGLShader::SetInt(const std::string& name, int value) {
+		UploadUniformInt(name, value);
+	}
+
+	void OpenGLShader::SetIntArray(const std::string& name, int* values, uint32_t count) {
+		UploadUniformIntArray(name, values, count);
+	}
+
+	void OpenGLShader::SetFloat(const std::string& name, float value) {
+		UploadUniformFloat(name, value);
+	}
+
+	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value) {
+		UploadUniformFloat3(name, value);
+	}
+
+	void OpenGLShader::SetFloat4(const std::string& name, const glm::vec4& value) {
+		UploadUniformFloat4(name, value);
+	}
+
+	void OpenGLShader::SetMat4(const std::string& name, const glm::mat4& value) {
+		UploadUniformMat4(name, value);
+	}
+
+	void OpenGLShader::UploadUniformInt(const std::string& name, int value) {
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1i(location, value);
+	}
+
+	void OpenGLShader::UploadUniformIntArray(const std::string& name, int* values, uint32_t count) {
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1iv(location, count, values);
+	}
+
+	void OpenGLShader::UploadUniformFloat(const std::string& name, float value) {
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1f(location, value);
+	}
+
+	void OpenGLShader::UploadUniformFloat2(const std::string& name, const glm::vec2& values) {
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform2f(location, values.x, values.y);
+	}
+
+	void OpenGLShader::UploadUniformFloat3(const std::string& name, const glm::vec3& values) {
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform3f(location, values.x, values.y, values.z);
+	}
+
+	void OpenGLShader::UploadUniformFloat4(const std::string& name, const glm::vec4& values) {
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform4f(location, values.x, values.y, values.z, values.w);
+	}
+
+	void OpenGLShader::UploadUniformMat3(const std::string& name, const glm::mat3& matrix) {
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniformMatrix3fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
+	}
+
+	void OpenGLShader::UploadUniformMat4(const std::string& name, const glm::mat4& matrix) {
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
+	}
 }
--- CetusEngine/src/Platform/OpenGL/OpenGLShader.h
+++ CetusEngine/src/Platform/OpenGL/OpenGLShader.h
@@ -12,6 +12,24 @@ namespace Cetus {
         virtual void Bind() const override;
         virtual void Unbind() const override;
 
+        void SetInt(const std::string &name, int value) override;
+        void SetIntArray(const std::string &name, int *values, uint32_t count) override;
+        void SetFloat(const std::string &name, float value) override;
+        void SetFloat3(const std::string &name, const glm::vec3 &value) override;
+        void SetFloat4(const std::string &name, const glm::vec4 &value) override;
+        void SetMat4(const std::string &name, const glm::mat4 &value) override;
+
+        void UploadUniformInt(const std::string& name, int value);
+        void UploadUniformIntArray(const std::string& name, int* values, uint32_t count);
+
+        void UploadUniformFloat(const std::string& name, float value);
+        void UploadUniformFloat2(const std::string& name, const glm::vec2& values);
+        void UploadUniformFloat3(const std::string& name, const glm::vec3& values);
+        void UploadUniformFloat4(const std::string& name, const glm::vec4& values);
+
+        void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
+        void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
+
     private:
         std::string ReadFile(const std::string& filepath);
         std::unordered_map<GLenum, std::string> PreProcess(const std::string& source);
